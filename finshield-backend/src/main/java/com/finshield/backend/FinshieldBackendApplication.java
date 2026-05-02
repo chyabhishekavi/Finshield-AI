cd663de110113dcec3f9d2cfeb8b06a84d4715d4
Abhishek <chyabhishekavi@gmail.com>
2026-05-02 12:00:00 +0530

    build(backend): initialize Spring Boot service

---
+++ finshield-backend/src/main/java/com/finshield/backend/FinshieldBackendApplication.java
@@ -0,0 +1,12 @@
+package com.finshield.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinshieldBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinshieldBackendApplication.class, args);
+    }
+}
